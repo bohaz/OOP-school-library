35e5de90c209d2aa9a39c74701b67d70ef25c0b4
Ricardo Martínez <ricardomartinezvet@gmail.com>
2023-09-05 15:16:07 -0300

    Modified person file

--- person.rb
+++ person.rb
@@ -1,6 +1,8 @@
-class Person
-  attr_accessor :name, :age
-  attr_reader :id
+require_relative 'nameable'
+
+class Person < Nameable
+  attr_reader :id, :name, :age
+  attr_writer :name, :age
 
   def initialize(age, name = 'Unknown', parent_permission: true)
     @id = rand(1..1000)
@@ -11,6 +13,10 @@ class Person
 
   def can_use_services?
     of_age? || @parent_permission
+  end
+
+  def correct_name
+    @name
   end
 
   private
