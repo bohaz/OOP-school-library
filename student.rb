ebf86c1c2a5a97db2de1fe14779573be3bb21111
Ricardo Martínez <ricardomartinezvet@gmail.com>
2023-09-06 15:11:17 -0300

    Update student and book for changes

--- student.rb
+++ student.rb
@@ -1,4 +1,5 @@
 require_relative 'person'
+attr_reader :classroom
 
 class Student < Person
   def initialize(age, classroom, name = 'Unknown', parent_permission: true)
@@ -9,5 +10,10 @@ class Student < Person
 
   def play_hooky
     '¯\\_(ツ)_/¯'
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 end
