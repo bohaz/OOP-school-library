4ff02a98b0c7e38d86361f056c61c72e8e0ac562
Ricardo Martínez <ricardomartinezvet@gmail.com>
2023-09-06 13:42:28 -0300

    Update person.rb file

--- person.rb
+++ person.rb
@@ -1,28 +1,18 @@
 require_relative 'nameable'
 
 class Person < Nameable
-  attr_accessor :name, :age
-  attr_reader :id
+  attr_reader :rentals
 
   def initialize(age, name = 'Unknown', parent_permission: true)
-    super()
-    @id = rand(1..1000)
-    @name = name
-    @age = age
-    @parent_permission = parent_permission
-  end
-
-  def can_use_services?
-    of_age? || @parent_permission
+    super(name, age, parent_permission)
+    @rentals = []
   end
 
   def correct_name
     @name
   end
 
-  private
-
-  def of_age?
-    @age >= 18
+  def add_rental(rental)
+    @rentals << rental
   end
 end
