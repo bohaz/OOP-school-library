8bf2526f60e5a3b8d0a4d9b606c94663be69b88d
Javier Vargas <Javieralejandroortga@gmail.com>
2023-09-13 13:44:31 -0400

    Make changes

---
+++ person.rb
@@ -0,0 +1,34 @@
+require_relative 'nameable'
+class Person < Nameable
+  attr_accessor :name, :age, :classroom
+  attr_reader :id, :rentals
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    super()
+    @id = rand(1..1000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+    @rentals = []
+  end
+  def can_use_services?
+    of_age? || @parent_permission
+  end
+  def correct_name
+    @name
+  end
+  def add_rental(rental)
+    rentals << rental
+  end
+  def to_h
+    {
+      'id' => @id,
+      'name' => @name,
+      'age' => @age,
+      'parent_permission' => @parent_permission
+    }
+  end
+  private
+  def of_age?
+    @age >= 18
+  end
+end
