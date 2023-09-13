babb59b57c372190e1e7a8ba30e8a8832cdb925a
Ricardo Martínez <ricardomartinezvet@gmail.com>
2023-09-13 20:23:57 -0300

    update rental

--- spec/rental_spec.rb
+++ spec/rental_spec.rb
@@ -1,4 +1,6 @@
 require_relative '../rental'
+require_relative '../book'
+require_relative '../person'
 
 describe Rental do
   let(:book) { Book.new('Animal Farm', 'George Orwell') }
@@ -7,5 +9,20 @@ describe Rental do
 
   it 'should have a date' do
     expect(subject.date).to eq('2023-09-13')
+  end
+
+  describe '#to_h' do
+    it 'returns a hash representation of the rental' do
+      allow(book).to receive(:title).and_return('Animal Farm')
+      allow(person).to receive(:id).and_return(1)
+      
+      expected_hash = {
+        'date' => '2023-09-13',
+        'book_title' => 'Animal Farm',
+        'person_id' => 1
+      }
+
+      expect(subject.to_h).to eq(expected_hash)
+    end
   end
 end
