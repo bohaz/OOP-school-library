9001f57fbde3eb7d101cdb21de3c162161a7fe66
Henok Mekonnen <henokmekonnen5@gmail.com>
2023-09-13 18:13:29 +0300

    update rental.rb to preserve data

--- rental.rb
+++ rental.rb
@@ -1,13 +1,18 @@
 class Rental
   attr_accessor :date
   attr_reader :book, :person
-
   def initialize(date, book, person)
     @date = date
     @book = book
     @person = person
-
     book.add_rental(self)
     person.add_rental(self)
+  end
+  def to_h
+    {
+      'date' => @date,
+      'book_title' => @book.title,
+      'person_id' => @person.id
+    }
   end
 end
